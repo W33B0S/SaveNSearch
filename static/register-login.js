9606271fd2189627416cda32290a2f750a2e694c
W33B0S <144830844+W33B0S@users.noreply.github.com>
2024-11-26 22:02:41 -0500

    Delete static directory

--- static/register-login.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-    const formContainer = document.getElementById('formContainer');
-    formContainer.classList.remove('hidden'); // Muestra el formulario al cargar
-};
