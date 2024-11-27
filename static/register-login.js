81026e5727ee976853db4f70b79de6584c077bc8
MARTIN <mlazo120203@gmail.com>
2024-11-26 19:57:42 -0500

    importacion

---
+++ static/register-login.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    const formContainer = document.getElementById('formContainer');
+    formContainer.classList.remove('hidden'); // Muestra el formulario al cargar
+};
