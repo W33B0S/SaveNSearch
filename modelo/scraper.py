7d06628dae67246700fc1a3f283adc5b11086811
W33B0S <144830844+W33B0S@users.noreply.github.com>
2024-11-28 17:54:27 -0500

    Update(!=tottus - cloud) scraper.py

--- modelo/scraper.py
+++ modelo/scraper.py
@@ -1,20 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 from passlib.hash import pbkdf2_sha256
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from modelo.paginas import plazavea, franco, tottus
-
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+from modelo.paginas import plazavea, franco
 
 class CatalogosScraper:
     def __init__(self, mysql):
         self.mysql = mysql
         self.plazavea_urls = plazavea
         self.franco_urls = franco
-        self.tottus_urls = tottus
 
     def scrape_plazavea(self, url, id_c):
         response = requests.get(url)
@@ -63,38 +56,7 @@ class CatalogosScraper:
 
             self.save_to_database(titulo2, precio2, hashed_prod2, 2, id_c)
 
-        return products2
-    
-    def scrape_tottus(self, url, id_c):
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.get(url)
-        products3 = []
-
-        namep3 = driver.find_elements(By.XPATH, '//b[starts-with(@id, "testId-pod-displaySubTitle-")]')
-        pricep3 = driver.find_elements(By.CLASS_NAME, 'copy10.primary.medium.jsx-3451706699.normal.line-height-22')
-        urlp3 = driver.find_elements(By.CSS_SELECTOR, 'a[data-pod="catalyst-pod"]')
-        imgp3 = driver.find_elements(By.CSS_SELECTOR, 'img.jsx-1996933093')
-
-        for producto in zip(namep3, pricep3, urlp3, imgp3):
-            name_p,price_p,url_p,img_p=producto
-            titulo3=name_p.text
-            precio3=price_p.text
-            producto_url3=url_p.get_attribute('href')
-            image_url3=img_p.get_attribute('src')
-            products3.append({
-                'titulo3': titulo3,
-                'precio3': precio3,
-                'producto_url3': producto_url3,
-                'image_url': image_url3
-            })
-                
-            hashed_prod3 = pbkdf2_sha256.hash(producto_url3)
-
-            self.save_to_database(titulo3, precio3, hashed_prod3, 3, id_c)
-
-        driver.quit()
-
-        return products3
+        return products
 
     def save_to_database(self, titulo, precio, hashed_prod, id_s, id_c):
         cursor = self.mysql.connection.cursor()
